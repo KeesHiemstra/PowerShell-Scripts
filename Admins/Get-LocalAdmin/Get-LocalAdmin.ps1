9cf575cf9e653109c6442e5116516bcbcf741f32
Kees Hiemstra <chi@xs4all.nl>
2018-10-09 18:55:50 +0200

    Initial

---
+++ Admins/Get-LocalAdmin/Get-LocalAdmin.ps1
@@ -0,0 +1,18 @@
+﻿#
+# Get-LocalAdmin.ps1
+#
+
+function Get-LocalAdmin
+{
+    param ($Computer)
+
+    $Admins = Get-WmiObject Win32_GroupUser –Computer $Computer
+    $Admins = $Admins |Where-Object {$_.GroupComponent –like '*"Administrators"'}
+
+    $Admins | ForEach-Object {
+        $_.PartComponent –match ".+Domain\=(.+)\,Name\=(.+)$" > $Nul
+        $Matches[1].Trim('"') + "\" + $Matches[2].Trim('"')
+    }
+}
+
+Get-LocalAdmin .
