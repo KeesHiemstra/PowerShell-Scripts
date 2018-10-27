69ccabf0828021df41ed6abcb290b23132dc9509
Kees Hiemstra <chi@xs4all.nl>
2018-10-27 14:02:05 +0200

    Updates

---
+++ Admins/AnalyseEnvironment/AnalyseEnvironment.ps1
@@ -0,0 +1,20 @@
+﻿#
+# AnalyseEnvironment.ps1
+#
+
+## Capture the evironment variables on the Clipboard
+Start-Process -FilePath "Cmd.exe" -ArgumentList "/C Set | Clip"
+
+## Dump the capture in $SetDump
+$SetDump = Get-Clipboard
+
+$SysVars = @{}
+foreach ($V in $SetDump) {
+	$W = $V.Split('=')
+	if($W -ne '') { 
+		$SysVars.Add($W[0], $W[1])
+		#"$($W[0]) ==> '$($W[1])'"
+	}
+}
+
+$SysVars[0]
---
+++ Markdown/Edit.md
@@ -0,0 +1,21 @@
+# Edit files
+
+## Windows
+
+### Visual Studo code
+
+Executable: "C:\Program Files\Microsoft VS Code\bin\code.cmd"
+
+Call: Code.cmd
+
+### Notepad++
+
+Executable: "C:\Program Files\Notepad++\notepad++.exe"
+
+Call: NEdit.cmd
+
+### Notepad
+
+Executable: "C:\Windows\notepad.exe" (default in Windows)
+
+Call: Nodepad.exe
